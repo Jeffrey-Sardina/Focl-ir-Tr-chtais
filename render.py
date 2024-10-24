affb4a6b2a2bc74611f38ede8196605e1841fb88
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2024-10-24 15:46:24 +0100

    much better md / tex rendering

--- render.py
+++ render.py
@@ -1,5 +1,6 @@
 import json
 import glob
+import sys
 
 def load_terms():
     terms = {}
@@ -7,27 +8,139 @@ def load_terms():
     for term_file in term_files:
         with open(term_file, 'r') as inp:
             term = json.load(inp)
-            terms[term['term']] = term
+            if not REVERSE:
+                terms[term['term']] = term
+            else:
+                terms[term['citation-form']] = term
     terms_sorted = {key:terms[key] for key in sorted(list(terms.keys()))}
-    return terms
+    return terms_sorted
+
+def bold(render_str):
+    if MODE == 'latex':
+        return f"\\textbf{{{render_str}}}"
+    else:
+        return f"**{render_str}**"
+    
+def italics(render_str):
+    if MODE == 'latex':
+        return f"\\textit{{{render_str}}}"
+    else:
+        return f"*{render_str}*"
+    
+def unordered_list(render_strs):
+    list_str = ""
+    if MODE == 'latex':
+        list_str += "\\begin{itemize}\n"
+        for render_str in render_strs:
+            list_str += f"\t\\item {render_str}\n"
+        list_str += "\\end{itemize}\n"
+    else:
+        for render_str in render_strs:
+            list_str += f"- {render_str}\n"
+    return list_str
+
+def ordered_list(render_strs):
+    list_str = ""
+    if MODE == 'latex':
+        list_str += "\\begin{enumerate}\n"
+        for render_str in render_strs:
+            list_str += f"\t\\item {render_str}\n"
+        list_str += "\\end{enumerate}\n"
+    else:
+        for i, render_str in enumerate(render_strs):
+            list_str += f"{i}. {render_str}\n"
+    return list_str
 
 def render_term(term):
     render_str = ""
 
     # term translation
-    render_str += f"**{term['term']}: {term['citation-form']}**\n"
+    if not REVERSE:
+        render_str += bold(f"{term['term']}: {term['citation-form']}")
+    else:
+        render_str += bold(f"{term['citation-form']}: {term['term']}")
+
+    render_str += "\n\n"
+
+    # term provenance
+    render_str += "tagairtí:\n"
+    prov_list = [f"{key}: {term['prov'][key]}" for key in term["prov"]]
+    render_str += unordered_list(prov_list)
+    render_str += "\n"
+
+    # term notes
+    render_str += "nótaí aistriúcháin:\n"
+    render_str += unordered_list(term["notes"])
+    render_str += "\n"
     
     # declension paradigm
-    render_str += f"- NS: {term['ns']}\n"
-    render_str += f"- GS: {term['gs']}\n"
-    render_str += f"- NP: {term['np']}\n"
-    render_str += f"- GP: {term['gp']}\n"
-    render_str += f"\n"
+    render_str += "leaganacha éagsúla:\n"
+    dec_list = [f"{key}: {term['declensions'][key]}" for key in term["declensions"]]
+    render_str += unordered_list(dec_list)
+    render_str += "\n"
 
     return render_str
 
+def render_table(terms):
+    if MODE == 'latex':
+        render_str = "\\begin{table}[!ht]\n"
+        render_str += "\t\\centering\n"
+        render_str += "\t\\begin{tabular}{|l|l|}\n"
+        render_str += "\t\\hline\n"
+        if not REVERSE:
+            render_str += "\t\t\\textbf{Béarla} & \\textbf{Gaeilge}\n"
+        else:
+            render_str += "\t\t\\textbf{Gaeilge} & \\textbf{Béarla}\n"
+        for term_id in terms:
+            term = terms[term_id]
+            if not REVERSE:
+                render_str += "\t\t" + bold(term['term']) + "&" + bold(term['citation-form']) + "\n"
+            else:
+                render_str += "\t\t" + bold(term['citation-form']) + "&" + bold(term['term']) + "\n"
+        render_str += "\t\\end{tabular}\n"
+        if not REVERSE:
+            render_str += "\\caption{Liosta na dtéarma Béarla ar fad agus a leagan Gaeilge, cuirtear in ord de réir na dtéarma Béarla.}\n"
+            render_str += "\\label{tab-terms-en-ga}\n"
+        else:
+            render_str += "\\caption{Liosta na dtéarma Béarla ar fad agus a leagan Gaeilge, cuirtear in ord de réir na dtéarma Gaeilge.}\n"
+            render_str += "\\label{tab-terms-ga-en}\n"
+        render_str += "\\end{table}"
+    else:
+        longest_en = 0
+        longest_ga = 0
+        for term_id in terms:
+            term = terms[term_id]
+            if len(term['term']) > longest_en:
+                longest_en = len(term['term'])
+            if len(term['citation-form']) > longest_ga:
+                longest_ga = len(term['citation-form'])
+        en_col = "**Béarla**".ljust(longest_en)
+        ga_col = "**Gaeilge**".ljust(longest_ga)
+        if not REVERSE:
+            render_str = f"|{en_col}|{ga_col}|\n"
+            render_str += f"|{'-'*longest_en}|{'-'*longest_ga}|\n"
+        else:
+            render_str = f"|{ga_col}|{en_col}|\n"
+            render_str += f"|{'-'*longest_ga}|{'-'*longest_en}|\n"
+        for term_id in terms:
+            term = terms[term_id]
+            en_col = term['term'].ljust(longest_en)
+            ga_col = term['citation-form'].ljust(longest_ga)
+            if not REVERSE:
+                render_str += f"|{en_col}|{ga_col}|\n"
+            else:
+                render_str += f"|{ga_col}|{en_col}|\n"
+    render_str += "\n"
+    return render_str
+
 if __name__ == '__main__':
+    MODE = sys.argv[1]
+    REVERSE = sys.argv[2] == '1'
+    assert MODE in ("latex", "markdown"), f"mode must be latex or maskdown but is {MODE}"
+
     terms = load_terms()
+    table_str = render_table(terms)
+    print(table_str)
     for term_id in terms:
         render_str = render_term(terms[term_id])
         print(render_str)
--- terms.md
+++ terms.md
@@ -1,7 +1,52 @@
-**computer science: ríomheolaíocht**
-- NS: an ríomheolaíocht mhór
-- GS: na ríomheolaíochta móire
-- NP: na ríomheolaíochtaí móra
-- GP: na ríomheolaíochtaí móra
+|**Gaeilge**         |**Béarla**      |
+|--------------------|----------------|
+|foghlaim na meaisíní|machine learning|
+|ríomheolaíocht      |computer science|
+|xxx                 |xxx             |
+
+
+**foghlaim na meaisíní: machine learning**
+
+tagairtí:
+- foghlaim: Ó Dónaill (1977), Dineen (1927)
+- meaisín: Ó Dónaill (1977)
+
+nótaí aistriúcháin:
+- Ní ghlactar le haistriúchán Téarma.ie (*meaisínfhoghlaim) toisc é a bheith ag úsáid comh-fhocal chun dhá ainmní a chur le chéile seachas an tuiseal ginideach (atá níos nádúrtha don Ghaeilge).
+- Úsáidtear 'na' ann toisc gur foghlaim í seo atá déanta ar mheaisíní i gcoitinne, seachas ar mheaisín(í) anaithnid(e). Féarch ar alt alt 1.15.(l) den Chaighdéan Oifigiúil le haghaidh tuilleadh eolais.
+
+leaganacha éagsúla:
+- ns: foghlaim éifeachtach na meaisíní
+- gs: foghlama éifeachtaí na meaisíní
+
+
+**ríomheolaíocht: computer science**
+
+tagairtí:
+- ríomheolaíocht: Ó Dónaill (1977)
+
+nótaí aistriúcháin:
+- Téarma díreach ar fáil i bhfoclóir iontaofa. Glactar leis.
+
+leaganacha éagsúla:
+- ns: an ríomheolaíocht mhaith
+- gs: na ríomheolaíochta maithe
+- np: na ríomheolaíochtaí maithe
+- gp: na ríomheolaíochtaí maithe
+
+
+**xxx: xxx**
+
+tagairtí:
+- xxx: xxx
+
+nótaí aistriúcháin:
+- xxx
+
+leaganacha éagsúla:
+- ns: xxx
+- gs: xxx
+- np: xxx
+- gp: xxx
 
 
