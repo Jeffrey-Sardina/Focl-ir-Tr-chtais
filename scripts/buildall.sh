99224e5e60f1a305eb0ed7842a2d2420b093f7b2
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2025-04-22 17:09:52 -0700

    sitemap scrip

--- scripts/buildall.sh
+++ scripts/buildall.sh
@@ -3,3 +3,5 @@
 python sitegen.py
 python render.py -nv
 python render.py -thesis
+./python-sitemap-generator/python-sitemap-generator.py "https://focloir-riomheolaiochta.github.io/" focloir
+./python-sitemap-generator/python-sitemap-generator.py "https://jeffrey-sardina.github.io/" jeffrey
