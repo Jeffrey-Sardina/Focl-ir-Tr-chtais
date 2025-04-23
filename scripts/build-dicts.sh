280a4ba3fd729061ca48b28ef370d8282db801e1
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2025-04-23 09:52:52 -0700

    scripts

---
+++ scripts/build-dicts.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+python sitegen.py
+python render.py -nv
+python render.py -thesis
+
--- scripts/build-sitemaps.sh
+++ scripts/build-sitemaps.sh
@@ -1,8 +1,4 @@
 #! /bin/bash
 
-python sitegen.py
-python render.py -nv
-python render.py -thesis
 python python-sitemap-generator/python-sitemap-generator.py "https://focloir-riomheolaiochta.github.io/" focloir
 python python-sitemap-generator/python-sitemap-generator.py "https://jeffrey-sardina.github.io/" jeffrey
-
